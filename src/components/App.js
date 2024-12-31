d3f2fe0b7ad233b4f9f4c167ac8ca05e9b0bde32
Ahmed_Yasser <ahmedyassertaha66@gmail.com>
2024-12-31 19:39:54 +0200

    project finished

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from 'react';
+
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+import NextButton from './NextButton';
+import Progress from './Progress';
+import FinishScreen from './FinishScreen';
+import Footer from './Footer';
+import Timer from './Timer';
+import { useQuizContext } from '../context/QuizContext';
+
+export default function App() {
+  const { dispatch, status } = useQuizContext();
+
+  useEffect(function () {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen />}
+        {status === 'active' && (
+          <>
+            <Progress />
+            <Question />
+            <Footer>
+              <Timer />
+              <NextButton />
+            </Footer>
+          </>
+        )}
+        {status === 'finished' && <FinishScreen />}
+      </Main>
+    </div>
+  );
+}
